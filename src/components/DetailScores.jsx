6fd530cf0e0062cb0daf68db81ae6798c702f512
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-05-20 21:04:21 -0400

    Delete favicons, added Keyboard event (almost failed)
    Delete favicons, added Keyboard event (almost failed)

--- src/components/DetailScores.jsx
+++ src/components/DetailScores.jsx
@@ -1,9 +1,10 @@
 /* Detail.jsx*/
 
+import { click } from "@testing-library/user-event/dist/click";
 import React from "react";
 
 // Components
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 
 import "./DetailScores.Module.css"
 
@@ -22,6 +23,32 @@ function DetailScores(){
         setClicked((value)=>value = event.target.id) // 버튼이 눌리면 setClicked를 이용해서 clicked의 값 업데이트
     }
 
+    // 키보드 이벤트
+    const handleKeyDown = (e) => {
+        //console.log(e);
+        if (e.key === '1' || e.key === '2' || e.key === '3' || e.key === '4' || e.key === '5') {
+            //console.log("Pressed")
+            // logic
+            //console.log(e.key)
+            const temp = document.getElementById(e.key);
+            //console.log(temp);
+            console.log("click")
+            temp.classList.add("clickedScore")
+        }
+      }
+      
+      // 시작하자마자, 키보드 이벤트 활성화
+      useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown)
+        //console.log("mount")
+        
+        return () => {
+          window.removeEventListener('keydown', handleKeyDown)
+          //console.log("unmount")
+        }
+      })
+
+
     // useEffect를 사용해서 clicked의 값이 업데이트 될때마다 아래의 함수를 실행시킨다.
     useEffect(() => {
         ClickedCircle(); // 버튼이 눌릴떄마다 업데이트
@@ -30,11 +57,11 @@ function DetailScores(){
         // 함수 로직
     function ClickedCircle() {
         // 공이 총 5개뿐이니까  1,2,3,4,5의 id를 가져온다.
-        for (let i = 1; i < 6; i++) {
+        for (let i = 1; i < 6; ++i) {
             // Id값이 숫자인 친구를 찾아서 보관 - score들에게 아이디를 1~5을 줬다.
             const temp = document.getElementById(i);
             // 클릭하면 위에 setClicked에서 눌린 target의 아이디를 주게되어있고, 1~5가 잘 들어온다.
-            //console.log(clicked);
+            console.log(clicked);
 
             // 클릭 로직
             // i 가 클릭보다 작거나 같을떄까지 공을 칠해주면된다.
@@ -52,12 +79,12 @@ function DetailScores(){
                 temp.classList.remove("clickedScore") 
                 }
             }
-            }
+        }
     };
 
     // map을 이용해서 각 요일들의 index에 접근할 예정
     const indexes = [1,2,3,4,5]
-    const Scores = indexes.map((value, index)=>(<div className="score" key={index} id={value} onClick={onClick}></div>))
+    const Scores = indexes.map((value, index)=>(<div className="score" key={index} id={value} onClick={onClick} ></div>))
 
     return(
         <>
